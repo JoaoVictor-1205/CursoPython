4d1c83622a177a178138e3faed457fb0e3bef11e
JoaoVictor-1205 <168592928+JoaoVictor-1205@users.noreply.github.com>
2026-02-11 11:10:50 -0300

    .

--- aula159.py
+++ aula159.py
@@ -5,6 +5,9 @@ class Pessoa:
     nome: str
     sobrenome: str
 
+    def __post_init__(self):
+        self.nome_completo = f'{self.nome} {self.sobrenome}'
+
     @property
     def nome_completo(self):
         return f'{self.nome} {self.sobrenome}'
