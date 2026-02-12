05aac53c36642914690c7e42287e8a53c885cd7a
JoaoVictor-1205 <168592928+JoaoVictor-1205@users.noreply.github.com>
2026-02-12 17:10:14 -0300

    .

--- aula161.py
+++ aula161.py
@@ -1,15 +1,27 @@
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass
+from collections.abc import Sequence
+from typing import Iterator
 
 @dataclass
-class MyList:
+class MyList(Sequence):
     def __init__(self, *args):
         self._data = {}
-        self.index = 0
+        self._index = 0
 
     def append(self, valor):
-        self._data[self.index] = valor
-        self.index += 1
+        self._data[self._index] = valor
+        self._index += 1
+
+    def __iter__(self):
+        ...
+
+    def __len__(self) -> int:
+        return self._index
+    
+    def __getitem__(self, index):
+        return self._data[index]
 
 lista = MyList()
 lista.append('Carro')
-print(lista._data)
+lista.append('Vassoura')
+print(lista.__getitem__(0))
