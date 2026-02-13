8108d20d5cb9f0a499f47dd004d11c5307c6f90d
JoaoVictor-1205 <168592928+JoaoVictor-1205@users.noreply.github.com>
2026-02-13 09:06:33 -0300

    .

--- aula161.py
+++ aula161.py
@@ -7,21 +7,42 @@ class MyList(Sequence):
     def __init__(self, *args):
         self._data = {}
         self._index = 0
+        self.next_index = 0
 
-    def append(self, valor):
-        self._data[self._index] = valor
-        self._index += 1
-
-    def __iter__(self):
-        ...
+    def append(self, *values):
+        for value in values:
+            self._data[self._index] = value
+            self._index += 1
 
     def __len__(self) -> int:
         return self._index
     
     def __getitem__(self, index):
         return self._data[index]
+    
+    def __setitem__(self, index, valor):
+        self._data[index] = valor;
+
+    
+    
+    def __next__(self):
+        if self.next_index >= self._index:
+            self.next_index = 0
+            raise StopIteration
+        
+        value = self._data[self.next_index]
+        self.next_index += 1
+        return value
+    
+    def __iter__(self) -> Iterator:
+        return self
 
 lista = MyList()
 lista.append('Carro')
-lista.append('Vassoura')
-print(lista.__getitem__(0))
+lista.append('Vassoura', 'Caderno')
+lista[2] = 'mustang'
+
+for item in lista:
+    print(item)
+for item in lista:
+    print(item)
